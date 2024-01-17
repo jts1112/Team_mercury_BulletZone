9b42ebb7167b6db039bf3cadab6395b733257470
Matt Plumlee <Matthew.Plumlee@unh.edu>
2024-01-17 17:28:30 -0500

    Updated build files to Gradle 8.2.1 and Spring Boot 3.2.1

--- server/src/main/java/edu/unh/cs/cs619/bulletzone/web/GamesController.java
+++ server/src/main/java/edu/unh/cs/cs619/bulletzone/web/GamesController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import com.google.common.base.Preconditions;
 
 import edu.unh.cs.cs619.bulletzone.model.Direction;
