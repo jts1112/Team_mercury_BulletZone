bb40e57416129901051a07478e4d685e6d096e7b
jts1112 <Jack.T.Schneider@unh.edu>
2024-03-17 10:59:35 -0400

    Added MultidexEnabled as True o the clientside Build.gradle. Anyone with build issues involing to many method references should result in succesful build

---
+++ client/src/main/java/edu/unh/cs/cs619/bulletzone/BulletZoneRestClient_Test.java
@@ -0,0 +1,49 @@
+package edu.unh.cs.cs619.bulletzone;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mock.*;
+import static org.mockito.junit.MockitoJUnitRunner.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import edu.unh.cs.cs619.bulletzone.rest.BulletZoneRestClient;
+import edu.unh.cs.cs619.bulletzone.rest.BulletZoneRestClient_;
+import edu.unh.cs.cs619.bulletzone.util.BooleanWrapper;
+import edu.unh.cs.cs619.bulletzone.util.LongWrapper;
+
+/**
+ * This Class will test the client side interaction of Users Leaving and Joining.
+ */
+public class BulletZoneRestClient_Test {
+    @Mock
+    BulletZoneRestClient_ bulletZoneRestClient;
+
+    @Test
+    public void BulletZoneRestClient_UserJoin_Success(){
+
+    }
+
+
+    @Test
+    public void BulletZoneRestClient_UserJoin_Failure(){
+
+    }
+
+
+    @Test
+    public void BulletZoneRestClient_UserLeave_Success(){
+
+    }
+
+    @Test
+    public void BulletZoneRestClient_UserLeave_Failure(){
+
+    }
+
+}
