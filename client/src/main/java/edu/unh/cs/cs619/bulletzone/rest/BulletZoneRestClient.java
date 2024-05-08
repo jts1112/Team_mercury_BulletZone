6b17e99789f0cadaeceddee68bad29ac162f7fc2
jec1151 <jec1151@usnh.edu>
2024-05-08 01:13:57 -0400

    Javadoc changes

--- client/src/main/java/edu/unh/cs/cs619/bulletzone/rest/BulletZoneRestClient.java
+++ client/src/main/java/edu/unh/cs/cs619/bulletzone/rest/BulletZoneRestClient.java
@@ -26,12 +26,12 @@ import edu.unh.cs.cs619.bulletzone.util.ResultWrapper;
  */
 
 //@Rest(rootUrl = "http://172.29.160.1:8080/games",
-//@Rest(rootUrl = "http://172.24.128.1:8080/games",
-@Rest(rootUrl = "http://172.16.32.123:8080/games", // Shane's ip
+//@Rest(rootUrl = "http://172.19.19.243:8080/games",
+//@Rest(rootUrl = "http://172.16.32.121:8080/games", // Shane's ip
 //@Rest(rootUrl = "http://10.21.95.181:8080/games",
 //@Rest(rootUrl = "http://10.21.173.197:8080/games",
 //@Rest(rootUrl = "http://10.21.164.194:8080/games",
-//@Rest(rootUrl = "http://stman1.cs.unh.edu:61905/games",
+@Rest(rootUrl = "http://stman1.cs.unh.edu:61905/games",
 //@Rest(rootUrl = "http://stman1.cs.unh.edu:6192/games",
 //@Rest(rootUrl = "http://stman1.cs.unh.edu:61902/games",
         converters = {StringHttpMessageConverter.class, MappingJackson2HttpMessageConverter.class}
